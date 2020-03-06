fc4396820b5c35a0593bdb66159bec8a587fef1e
j-blank <dickyjblank@gmail.com>
2020-03-06 11:07:00 +0700

    deleting useless line

--- bot.js
+++ bot.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const http = require('http');
 const twilio = require('twilio');
-// const fetch = require('node-fetch');
 const axios = require('axios')
 
 const app = express();
-// const router = express.Router();
 const MessagingResponse = twilio.twiml.MessagingResponse;
 const PORT = 3000
 
 app.set('port', PORT);
-// app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/', async (req,res) => {
@@ -25,20 +22,10 @@ app.post('/', async (req,res) => {
         }
     }
     else if (req.body.Body == '/cat') {
-        // if (req.body.NumMedia > 0) {
-        // let cat = "https://images.unsplash.com/photo-1518717758536-85ae29035b6d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
         message = new MessagingResponse().message('this is some random image of cat for you').media('https://cataas.com/cat')
-        // }
-        // else {
-        //     MessagingResponse.message('cannot show image');
-        // }
-        // res.set('Content-Type', 'text/xml');
-        // res.status(200).send(MessagingResponse.toString());
     }
     else {
         message = new MessagingResponse().message('I\'m sorry i don\'t understand')
-        // res.set('Content-Type', 'text/xml');
-        // res.status(200).send(MessagingResponse.toString())
     }
     res.set('Content-Type', 'text/xml');
     res.status(200).send(await message.toString())
